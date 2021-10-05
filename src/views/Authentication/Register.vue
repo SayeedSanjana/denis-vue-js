8ea424754e131242319e1b7243cafd3f7748d659
unknown <sanjana.sayeed68@gmail.com>
2021-10-05 11:23:57 +0600

    registration design

--- src/views/Authentication/Register.vue
+++ src/views/Authentication/Register.vue
@@ -4,31 +4,32 @@
 
  
 
-  <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto  md:w-1/2 xl:w-1/2 h-screen px-6 lg:px-16 xl:px-12
+  <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto  md:w-1/2 xl:w-1/2 h-screen px-6 lg:px-16 xl:px-12 xl:ml-20
         flex items-center justify-center">
 
     <div class="w-full h-100">
 
-
-      <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12">Log in to your account</h1>
+      <h1 class="text-xl md:text-3xl font-bold leading-tight mt-12 text-left " style="color:#005072">MT Dental Center</h1>
+      <h1 class="text-xl md:text-3xl font-bold leading-tight mt-3 text-left  mb-10"  style="color:#005072">Sign Up</h1>
 
           <form action="" @submit.prevent="signupForm">
                 <div>
                     <div class="flex -mx-3">
-                        <div class="w-1/2 px-3 mb-5">
+                        <div class="w-full px-3 mb-5 text-left">
                             <label for="" class="text-xs font-semibold px-1">Full name</label>
                             <div class="flex">
                                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                                <input v-model.trim="formData.name" type="text" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="John">
+                                <input v-model.trim="formData.name" type="text" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-1 outline-none" style="background:#E7FBFC" >
                             </div>
                         </div>
-                      
-                   
-                        <div class="w-1/2 px-3 mb-5">
+                    </div>
+                    <div class="flex -mx-3">
+                        
+                        <div class="w-1/2 px-3 mb-5 text-left">
                             <label for="" class="text-xs font-semibold px-1">Gender</label>
                             <div class="relative">
               <select
-                class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                class="block appearance-none w-full  text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white" style="background:#E7FBFC"
                 id="gender" v-model="formData.gender">
                 <option>Male</option>
                 <option>Female</option>
@@ -41,11 +42,7 @@
               </div>
             </div>
                         </div>
-                      
-                    
-                    </div>
-                    <div class="flex -mx-3">
-                        <div class="w-1/2 px-3 mb-5">
+                        <div class="w-1/2 px-3 mb-5 text-left">
                             <label for="" class="text-xs font-semibold px-1">Date of Birth</label>
                             <div class="">
                                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
@@ -53,20 +50,9 @@
                             id="date" type="date" placeholder="" v-model="formData.dob">
                             </div>
                         </div>
-                      
-                        <div class="w-1/2 px-3 mb-5">
-                            <label for="" class="text-xs font-semibold px-1">Phone</label>
-                            <div class="flex">
-                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                                <input v-model.trim="formData.phone" type="text" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Phone Number">
-                            </div>
-                        </div>
                     </div>
-                   
-                      
-                   
                     <div class="flex -mx-3">
-                        <div class="w-full px-3 mb-5">
+                        <div class="w-full px-3 mb-5 text-left">
                             <label for="" class="text-xs font-semibold px-1">Address</label>
                             <div class="flex">
                                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
@@ -76,7 +62,16 @@
                       
                     </div>
                     <div class="flex -mx-3">
-                        <div class="w-full px-3 mb-5">
+                    <div class="w-full px-3 mb-5 text-left">
+                            <label for="" class="text-xs font-semibold px-1">Phone</label>
+                            <div class="flex">
+                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
+                                <input v-model.trim="formData.phone" type="text" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Phone Number">
+                            </div>
+                        </div>
+                     </div>
+                    <div class="flex -mx-3">
+                        <div class="w-full px-3 mb-5 text-left">
                             <label for="email" class="text-xs font-semibold px-1">Email</label>
                             <div class="flex">
                                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
@@ -85,7 +80,7 @@
                         </div>
                     </div>
                     <div class="flex -mx-3">
-                        <div class="w-full px-3 mb-12">
+                        <div class="w-full px-3 mb-12 text-left">
                             <label for="password" class="text-xs font-semibold px-1">Password</label>
                             <div class="flex">
                                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
@@ -93,6 +88,8 @@
                             </div>
                         </div>
                     </div>
+
+                    
                     <div class="flex -mx-3">
                         <div class="w-full px-3 mb-5">
                             <button class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">REGISTER NOW</button>
@@ -111,7 +108,7 @@
     </div>
   </div>
    <div class="hidden lg:block w-full md:w-1/2 xl:w-1/2 h-screen">
-    <img src="@/assets/svgs/register.svg" alt="" class="w-full h-full object-cover">
+    <img src="@/assets/svgs/register.svg" alt="" class="w-full h-full object-cover flex justify-end object-right">
   </div>
 
 </section>     
