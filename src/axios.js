0b012b366888e0ac194270eeb5c2b6dc8f1c8fc6
SayeedSanjana <65913030+SayeedSanjana@users.noreply.github.com>
2021-10-27 13:17:07 +0600

    ip address changed

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios"
 
- //axios.defaults.baseURL='http://178.128.127.150:81/api/';
-axios.defaults.baseURL='http://localhost:3000/api/';
+ axios.defaults.baseURL='http://178.128.127.150:81/api/';
+//axios.defaults.baseURL='http://localhost:3000/api/';
