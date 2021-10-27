793872731ead96fc7be669a9de2d637e6f07331d
SayeedSanjana <65913030+SayeedSanjana@users.noreply.github.com>
2021-10-27 14:42:49 +0600

    back to normal axios file

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,15 @@
 import axios from "axios"
+// import 'dotenv/config';
+// let ax = axios;
 
- axios.defaults.baseURL='http://178.128.127.150:81/api/';
-//axios.defaults.baseURL='http://localhost:3000/api/';
+// const NODE_ENV = process.env.NODE_ENV; 
+
+// console.log(process.env.DEV);
+// if ( NODE_ENV == 'development') {
+   
+    axios.defaults.baseURL= 'http://localhost:3000/api/';
+// } else {
+    
+    axios.defaults.baseURL= 'http://178.128.127.150:81/api/';
+    //  axios.defaults.baseURL='http://178.128.127.150:81/api/';
+//}
