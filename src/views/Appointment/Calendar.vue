90c996950ef6974aad798d3405972ab9e0f0ac3d
SayeedSanjana <65913030+SayeedSanjana@users.noreply.github.com>
2021-11-28 18:52:41 +0600

    calender

--- src/views/Appointment/Calendar.vue
+++ src/views/Appointment/Calendar.vue
@@ -1,58 +1,144 @@
 <template>
     <div>
-        <div class="flex overflow-x-scroll pb-10 hide-scroll-bar">
+        <div class="flex overflow-x-scroll pb-5 hide-scroll-bar border-b border-regal-blue border-opacity-30">
+          {{this.dateArr[0]}}
             <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
                 <div class="inline-block px-3" v-for="i in 6" :key="i">
-                    <div
-                        class="text-regal-teal text-left mt-12 px-3 w-64 h-32 max-w-xs overflow-hidden  rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                    <div class="text-regal-teal text-left mt-12 px-3 w-64 h-32 max-w-xs overflow-hidden  rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
                         <h1 class="font-semibold mt-4">Patient A</h1>
                         <p class="text-sm mt-2">09876543212</p>
                         <div class="flex">
-                            <div>
-                                <p class="text-xs mt-6">Visiting time: 8:00:00-8:30:00</p>
+                          <div>
+                             <div class="text-xs mt-6">
+                              Visiting time: 8:00:00-8:30:00
                             </div>
-                            <div
-                                class="border border-regal-blue rounded-full text-gray-600  bg-white hover:bg-white focus:outline-none appearance-none text-center">
-                                Scheduled
+                          </div>
+                            <div class="mt-5 ml-2">
+                               <div class="border text-xs border-regal-blue rounded-full  px-0.5 py-0.5  bg-regal-scheduled text-white focus:outline-none appearance-none text-center">Scheduled</div> 
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- <div class="inline-block px-3">
-            <div
-              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
-            ></div>
-          </div>
-          <div class="inline-block px-3">
-            <div
-              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
-            ></div>
-          </div>
-          <div class="inline-block px-3">
-            <div
-              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
-            ></div>
-          </div>
-          <div class="inline-block px-3">
-            <div
-              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
-            ></div>
-          </div>
-          <div class="inline-block px-3">
-            <div
-              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
-            ></div>
-          </div>
-          <div class="inline-block px-3">
-            <div
-              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
-            ></div>
-          </div>
-          <div class="inline-block px-3">
-            <div
-              class="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
-            ></div>
-          </div> -->
+            </div>
+        </div>
+        <div class="flex overflow-x-scroll pb-5 hide-scroll-bar border-b border-regal-blue border-opacity-30">
+            <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
+                <div class="inline-block px-3" v-for="i in 6" :key="i">
+                    <div class="text-regal-teal text-left mt-12 px-3 w-64 h-32 max-w-xs overflow-hidden  rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                        <h1 class="font-semibold mt-4">Patient A</h1>
+                        <p class="text-sm mt-2">09876543212</p>
+                        <div class="flex">
+                          <div>
+                             <div class="text-xs mt-6">
+                              Visiting time: 8:00:00-8:30:00
+                            </div>
+                          </div>
+                            <div class="mt-5 ml-2">
+                               <div class="border text-xs border-regal-blue rounded-full  px-0.5 py-0.5  bg-regal-scheduled text-white focus:outline-none appearance-none text-center">Scheduled</div> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex overflow-x-scroll pb-5 hide-scroll-bar border-b border-regal-blue border-opacity-30">
+            <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
+                <div class="inline-block px-3" v-for="i in 6" :key="i">
+                    <div class="text-regal-teal text-left mt-12 px-3 w-64 h-32 max-w-xs overflow-hidden  rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                        <h1 class="font-semibold mt-4">Patient A</h1>
+                        <p class="text-sm mt-2">09876543212</p>
+                        <div class="flex">
+                          <div>
+                             <div class="text-xs mt-6">
+                              Visiting time: 8:00:00-8:30:00
+                            </div>
+                          </div>
+                            <div class="mt-5 ml-2">
+                               <div class="border text-xs border-regal-blue rounded-full  px-0.5 py-0.5  bg-regal-scheduled text-white focus:outline-none appearance-none text-center">Scheduled</div> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+         <div class="flex overflow-x-scroll pb-5 hide-scroll-bar border-b border-regal-blue border-opacity-30">
+            <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
+                <div class="inline-block px-3" v-for="i in 6" :key="i">
+                    <div class="text-regal-teal text-left mt-12 px-3 w-64 h-32 max-w-xs overflow-hidden  rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                        <h1 class="font-semibold mt-4">Patient A</h1>
+                        <p class="text-sm mt-2">09876543212</p>
+                        <div class="flex">
+                          <div>
+                             <div class="text-xs mt-6">
+                              Visiting time: 8:00:00-8:30:00
+                            </div>
+                          </div>  
+                            <div class="mt-5 ml-2">
+                               <div class="border text-xs border-regal-blue rounded-full  px-0.5 py-0.5  bg-regal-scheduled text-white focus:outline-none appearance-none text-center">Scheduled</div> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+         <div class="flex overflow-x-scroll pb-5 hide-scroll-bar border-b border-regal-blue border-opacity-30">
+            <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
+                <div class="inline-block px-3" v-for="i in 6" :key="i">
+                    <div class="text-regal-teal text-left mt-12 px-3 w-64 h-32 max-w-xs overflow-hidden  rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                        <h1 class="font-semibold mt-4">Patient A</h1>
+                        <p class="text-sm mt-2">09876543212</p>
+                        <div class="flex">
+                          <div>
+                             <div class="text-xs mt-6">
+                              Visiting time: 8:00:00-8:30:00
+                            </div>
+                          </div>    
+                            <div class="mt-5 ml-2">
+                               <div class="border text-xs border-regal-blue rounded-full  px-0.5 py-0.5  bg-regal-scheduled text-white focus:outline-none appearance-none text-center">Scheduled</div> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+         <div class="flex overflow-x-scroll pb-5 hide-scroll-bar border-b border-regal-blue border-opacity-30">
+            <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
+                <div class="inline-block px-3" v-for="i in 6" :key="i">
+                    <div class="text-regal-teal text-left mt-12 px-3 w-64 h-32 max-w-xs overflow-hidden  rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                        <h1 class="font-semibold mt-4">Patient A</h1>
+                        <p class="text-sm mt-2">09876543212</p>
+                        <div class="flex">
+                          <div>
+                             <div class="text-xs mt-6">
+                              Visiting time: 8:00:00-8:30:00
+                            </div>
+                          </div>
+                            <div class="mt-5 ml-2">
+                               <div class="border text-xs border-regal-blue rounded-full  px-0.5 py-0.5  bg-regal-scheduled text-white focus:outline-none appearance-none text-center">Scheduled</div> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+         <div class="flex overflow-x-scroll pb-5 hide-scroll-bar ">
+            <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
+                <div class="inline-block px-3" v-for="i in 6" :key="i">
+                    <div class="text-regal-teal text-left mt-12 px-3 w-64 h-32 max-w-xs overflow-hidden  rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                        <h1 class="font-semibold mt-4">Patient A</h1>
+                        <p class="text-sm mt-2">09876543212</p>
+                        <div class="flex">
+                          <div>
+                             <div class="text-xs mt-6">
+                              Visiting time: 8:00:00-8:30:00
+                            </div>
+                          </div>   
+                            <div class="mt-5 ml-2">
+                               <div class="border text-xs border-regal-blue rounded-full  px-0.5 py-0.5  bg-regal-scheduled text-white focus:outline-none appearance-none text-center">Scheduled</div> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -63,21 +149,32 @@
         created() {
             this.date1()
         },
+        data(){
+          return{
+           dateArr:[]
+          }
+        },
         methods: {
             date1() {
                 let date = new Date();
 
-                let dateArr = [];
-                dateArr[0] = new Date();
+               // let dateArr = [];
+                this.dateArr[0] = new Date();
                 for (let i = 0; i < 6; i++) {
                     let nextDate = date.setDate(date.getDate() + 1)
-                    dateArr.push(nextDate);
+                    let first_date = new Date(nextDate);
+                    let start_date = first_date.toLocaleDateString().slice(0,10);
+                    console.log(start_date)
+                    // nextDate=nextDate.toString();
+                    // nextDate=nextDate.slice(0, 5)
+                    // console.log(nextDate)
+                    this.dateArr.push(nextDate).toString().slice(0,9);
                     // console.log(new Date(nextDate));
                 }
 
-                for (let i = 0; i < dateArr.length; i++) {
+                // for (let i = 0; i < dateArr.length; i++) {
                     
-                    console.log( new Date(dateArr[i]));
+                //     console.log( new Date(dateArr[i]));
                     // console.log(new Date(nextDate));
                 }
             },
@@ -99,7 +196,6 @@
             
         }
 
-    }
 </script>
 
 <style scoped>
